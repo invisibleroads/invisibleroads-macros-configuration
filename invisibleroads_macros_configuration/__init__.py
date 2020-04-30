dd5cc6ffac4e02840f4be96e69ee4f9317fbc48a
Roy Hyunjin Han <rhh@crosscompute.com>
2020-04-29 21:02:32 -0400

    Split packages

--- invisibleroads_macros_configuration/__init__.py
+++ invisibleroads_macros_configuration/__init__.py
@@ -1,28 +1,4 @@
-import json
-from invisibleroads_macros_log import get_log
-from os.path import expandvars
+from .settings import Settings, set_default
+from .formats import load_json
 
-
-L = get_log(__name__)
-
-
-def expand_environment_variables(settings):
-    return {
-        k: expandvars(v) if isinstance(v, str) else v
-        for k, v in settings.items()}
-
-
-def set_default(settings, key, default, parse=None):
-    value = settings.get(key, default)
-    if key not in settings:
-        L.warning(f'using default {key} = {value}')
-    elif value in ('', None):
-        L.warning(f'missing {key}')
-    elif parse:
-        value = parse(value)
-    settings[key] = value
-    return value
-
-
-def load_json(path):
-    return json.load(open(path, 'rt'))
+# flake8: noqa: E401
